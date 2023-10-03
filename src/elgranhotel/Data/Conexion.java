d0869444b4453e9f8240583118dd904eee470a24
LeoHuari <leohuari2406@hotmail.com>
2023-10-03 12:01:51 -0300

    Cambios en Huesped y gitignore

--- src/elgranhotel/Data/Conexion.java
+++ src/elgranhotel/Data/Conexion.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package elgranhotel.Data;
 
-/**
- *
- * @author Leo Huari
- */
-public class Conexion {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public abstract class Conexion {
     
+    protected Connection conexion = null;
+    protected ResultSet resultado = null;
+    protected PreparedStatement sentencia = null;
+
+    private final String USER = "root";
+    private final String PASSWORD = "";
+    private final String DATABASE = "elgranhotel";
+    private final String DRIVER = "org.mariadb.jdbc.Driver";
+    
+    protected void conectarBase(){
+        if(conexion == null){
+            try {
+                Class.forName(DRIVER);
+                String urlBaseDeDatos = "jdbc:mariadb://localhost:3306/" + DATABASE;
+                conexion = DriverManager.getConnection(urlBaseDeDatos, USER, PASSWORD);
+            } catch (ClassNotFoundException | SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectar al Driver: "+ex);
+            }
+        }
+    }
+    
+    protected void desconectarBase(){
+         try {
+            if (resultado != null) {
+                resultado.close();
+            }
+            if (sentencia != null) {
+                sentencia.close();
+            }
+            if (conexion != null) {
+                conexion.close();
+                conexion = null;
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error al desconectar de la base: "+ex);
+        }
+    }
 }
--- src/elgranhotel/Entidades/Huesped.java
+++ src/elgranhotel/Entidades/Huesped.java
@@ -1,14 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package elgranhotel.Entidades;
 
-/**
- *
- * @author Leo Huari
- */
+
 public class Huesped {
+    private int idHuesped;
+    private String nombre;
+    private int dni;
+    private String correo;
+    private int celular;
+    private int domicilio;
+    private boolean estado;
+
+    public Huesped(int idHuesped, String nombre, int dni, String correo, int celular, int domicilio, boolean estado) {
+        this.idHuesped = idHuesped;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.correo = correo;
+        this.celular = celular;
+        this.domicilio = domicilio;
+        this.estado = estado;
+    }
+
+    public Huesped(String nombre, int dni, String correo, int celular, int domicilio, boolean estado) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.correo = correo;
+        this.celular = celular;
+        this.domicilio = domicilio;
+        this.estado = estado;
+    }
+
+    public Huesped() {
+    }
+
+    public int getIdHuesped() {
+        return idHuesped;
+    }
+
+    public void setIdHuesped(int idHuesped) {
+        this.idHuesped = idHuesped;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public int getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(int domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
     
 }
