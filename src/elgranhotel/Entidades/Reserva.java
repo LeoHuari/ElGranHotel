b8faaab2c5163be2355b0427aa255466806ee103
CristianMarch <kicho2002@hotmail.com>
2023-10-03 12:18:58 -0300

    Agrego entidad Reserva

---
+++ src/elgranhotel/Entidades/Reserva.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package elgranhotel.Entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author kicho
+ */
+public class Reserva {
+    private Huesped huesped;
+    private int cantPersonas;
+    private LocalDate fechaEntrada;
+    private LocalDate fechaSalida;
+    
+}
