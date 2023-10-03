7be8278b148a18723dc037c22ddd8ee53fde6f84
LeoHuari <leohuari2406@hotmail.com>
2023-10-03 12:15:46 -0300

    Se agrega clase TipoHabitacion

---
+++ src/elgranhotel/Entidades/TipoHabitacion.java
@@ -0,0 +1,70 @@
+
+package elgranhotel.Entidades;
+
+
+public class TipoHabitacion {
+    private int codigo;
+    private int cantidadPersonas;
+    private int cantidadCamas;
+    private String tipoCama;
+    private double precio;
+
+    public TipoHabitacion(int codigo, int cantidadPersonas, int cantidadCamas, String tipoCama, double precio) {
+        this.codigo = codigo;
+        this.cantidadPersonas = cantidadPersonas;
+        this.cantidadCamas = cantidadCamas;
+        this.tipoCama = tipoCama;
+        this.precio = precio;
+    }
+
+    public TipoHabitacion(int cantidadPersonas, int cantidadCamas, String tipoCama, double precio) {
+        this.cantidadPersonas = cantidadPersonas;
+        this.cantidadCamas = cantidadCamas;
+        this.tipoCama = tipoCama;
+        this.precio = precio;
+    }
+
+    public TipoHabitacion() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCantidadPersonas() {
+        return cantidadPersonas;
+    }
+
+    public void setCantidadPersonas(int cantidadPersonas) {
+        this.cantidadPersonas = cantidadPersonas;
+    }
+
+    public int getCantidadCamas() {
+        return cantidadCamas;
+    }
+
+    public void setCantidadCamas(int cantidadCamas) {
+        this.cantidadCamas = cantidadCamas;
+    }
+
+    public String getTipoCama() {
+        return tipoCama;
+    }
+
+    public void setTipoCama(String tipoCama) {
+        this.tipoCama = tipoCama;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
