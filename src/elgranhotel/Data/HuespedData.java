85b045a66eff431cd78315a663ed3567765cc81c
CristianMarch <kicho2002@hotmail.com>
2023-10-04 12:42:37 -0300

    banearHuesped añadido

--- src/elgranhotel/Data/HuespedData.java
+++ src/elgranhotel/Data/HuespedData.java
@@ -88,7 +88,7 @@ public class HuespedData extends Conexion {
         }
     }
 
-    public ArrayList<Huesped> BuscarHuespedPorId(int id) {
+    public Huesped BuscarHuespedPorId(int id) {
         String sql = " SELECT nombre, dni, correo, celular, domicilio FROM huesped WHERE idHuesped = ? AND estado = 1";
         try {
             conectarBase();
@@ -97,16 +97,17 @@ public class HuespedData extends Conexion {
             resultado = sentencia.executeQuery();
             if (resultado.next()) {
                 huesped = new Huesped();
-                huesped.setNombre();
-            sentencia.setInt(2, huesped.getDni());
-            sentencia.setString(3, huesped.getCorreo());
-            sentencia.setInt(4, huesped.getCelular());
-            sentencia.setString(5, huesped.getDomicilio());
-            sentencia.setBoolean(5, huesped.isEstado());
-            sentencia.executeUpdate(sql);
-                
+                huesped.setIdHuesped(id);
+                huesped.setNombre(resultado.getString("nombre"));
+                huesped.setDni(resultado.getInt("dni"));
+                huesped.setCorreo(resultado.getString("correo"));
+                huesped.setCelular(resultado.getInt("celular"));
+                huesped.setDomicilio(resultado.getString("domicilio"));
+                huesped.setEstado(true);
+            } else {
+                JOptionPane.showMessageDialog(null, "no existe el Huesped");
             }
-             } catch (Exception ex) {
+        } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno");
 
         } finally {
@@ -116,15 +117,115 @@ public class HuespedData extends Conexion {
                 JOptionPane.showMessageDialog(null, "Error al desconectar de la base " + ex);
             }
         }
-        return null;
+        return huesped;
     }
 
-    public ArrayList<Huesped> BuscarHuespedPorDNI(int DNI) {
-        return null;
+    public Huesped BuscarHuespedPorDNI(int dni) {
+        String sql = " SELECT nombre, dni, correo, celular, domicilio FROM huesped WHERE dni = ?";
+        try {
+            conectarBase();
+            sentencia = conexion.prepareStatement(sql);
+            sentencia.setInt(1, dni);
+            resultado = sentencia.executeQuery();
+            if (resultado.next()) {
+                huesped = new Huesped();
+                huesped.setIdHuesped(resultado.getInt("idHuesped"));
+                huesped.setNombre(resultado.getString("nombre"));
+                huesped.setDni(dni);
+                huesped.setCorreo(resultado.getString("correo"));
+                huesped.setCelular(resultado.getInt("celular"));
+                huesped.setDomicilio(resultado.getString("domicilio"));
+                huesped.setEstado(true);
+            } else {
+                JOptionPane.showMessageDialog(null, "no existe el Huesped");
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Huesped");
+
+        } finally {
+            try {
+                desconectarBase();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error al desconectar de la base " + ex);
+            }
+        }
+        return huesped;
     }
 
     public ArrayList<Huesped> ListarHuesped(Huesped huesped) {
-        return null;
+        String sql = " SELECT nombre, dni, correo, celular, domicilio FROM huesped WHERE estado = 1";
+        ArrayList<Huesped> huespedes = new ArrayList<>();
+
+        try {
+            conectarBase();
+            sentencia = conexion.prepareStatement(sql);
+            resultado = sentencia.executeQuery();
+            while (resultado.next()) {
+                huesped = new Huesped();
+                huesped.setIdHuesped(resultado.getInt("idHuesped"));
+                huesped.setNombre(resultado.getString("nombre"));
+                huesped.setDni(resultado.getInt("dni"));
+                huesped.setCorreo(resultado.getString("correo"));
+                huesped.setCelular(resultado.getInt("celular"));
+                huesped.setDomicilio(resultado.getString("domicilio"));
+                huesped.setEstado(true);
+
+                huespedes.add(huesped);
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Huesped " + ex);
+
+        } finally {
+            try {
+                desconectarBase();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error al desconectar de la base " + ex);
+            }
+        }
+        return huespedes;
     }
 
+    public void banearHuesped(int id) {
+        String sqlSelect = "SELECT estado FROM huesped where idHuesped = ?";
+        String sqlUpdate = "Update huesped Set estado = 0 WHERE idHuesped = ?";
+
+        try {
+            conectarBase();
+
+            sentencia = conexion.prepareStatement(sqlSelect);
+            sentencia.setInt(1, id);
+            resultado = sentencia.executeQuery();
+             if (resultado.next()) {
+                huesped = new Huesped();
+                huesped.setEstado(resultado.getBoolean("estado"));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error al conectar a la base de datos");
+        }
+        
+        if (huesped.isEstado()){
+        try {
+            sentencia = conexion.prepareStatement(sqlUpdate);
+            sentencia.setInt(1, id);
+            int exito = sentencia.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "Alumno añadido a la lista negra");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error al acceder a la tabla huesped");
+        } finally {
+            try {
+                desconectarBase();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error al desconectar de la base " + ex);
+            }
+        }
+        }else{
+            try {
+                desconectarBase();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "Error al desconectar de la base " + ex);
+            }
+        }
+    }
 }
